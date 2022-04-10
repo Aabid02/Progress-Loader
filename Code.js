16e71de9d281df7993986e300decf01ddd4b1d8a
Aabid <abidjamil18@gmail.com>
2022-04-10 20:46:12 +0530

    Add files via upload
    Check the working code  here - https://jsfiddle.net/Aabid02/908w3bLu/12

---
+++ Code.js
@@ -0,0 +1,12 @@
+const numb = document.querySelector(".key");
+let counter = 0;
+
+setInterval(() => {
+  if(counter==90){
+    clearInterval();
+  }
+  else{
+    counter += 1;
+    numb.textContent = counter+"%";
+  }
+}, 80);
